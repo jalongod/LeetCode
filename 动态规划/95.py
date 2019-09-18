685feaf311c0c8f070fa6fb337f9675ccfaba43c
zcr <zhangchengrong@sdjictec.com>
2019-09-18 11:39:40 +0800

    双指针

---
+++ 动态规划/95.py
@@ -0,0 +1,27 @@
+'''
+95. 不同的二叉搜索树 II
+给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+
+'''
+
+# Definition for a binary tree node.
+
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+
+        pass
+
+
+sol = Solution()
+max = sol.numTrees(4)
+print(max)
--- 双指针/27.py
+++ 双指针/27.py
@@ -5,24 +5,27 @@
 
 from typing import List
 
+
 class Solution:
     def removeElement2(self, nums: List[int], val: int) -> int:
-        i,n = 0,len(nums)
+        i, n = 0, len(nums)
         for j in range(n):
-            if nums[j]!=val:
-                nums[i]=nums[j]
-                i+=1
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
         return i
+
     def removeElement(self, nums: List[int], val: int) -> int:
-        i,n = 0,len(nums)
-        while i<n:
+        i, n = 0, len(nums)
+        while i < n:
             if nums[i] == val:
-                nums[i]=nums[n-1]
-                n-=1
+                nums[i] = nums[n - 1]
+                n -= 1
             else:
-                i+=1
+                i += 1
         return n
 
+
 sol = Solution()
-max = sol.removeElement([0,1,2,2,3,0,4,2],2)
+max = sol.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
 print(max)
