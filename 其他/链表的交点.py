9a99688dd1494943194e6f1348805c338ebdcb99
zcr <zhangchengrong@sdjictec.com>
2019-09-29 09:50:03 +0800

    链表交点

--- 其他/链表的交点.py
+++ 其他/链表的交点.py
@@ -12,11 +12,68 @@ class ListNode:
 class Solution:
     # 在确认有交点的情况下，查找两个链表的交点 用栈 或者比较长度
     def findJiaoDian(self, l1: ListNode, l2: ListNode) -> ListNode:
+        def sol1(l1, l2):
+            len1, len2, h1, h2 = 0, 0, l1, l2
+            while h1.next:
+                h1 = h1.next
+                len1 += 1
+                pass
+            while h2.next:
+                h2 = h2.next
+                len2 += 1
+            if len1 > len2:
+                h1 = l1
+                minus = len1 - len2
+                while minus > 0:
+                    minus -= 1
+                    h1 = h1.next
+                return h1
+            elif len2 > len1:
+                h2 = l2
+                minus = len2 - len1
+                while minus > 0:
+                    minus -= 1
+                    h2 = h2.next
+                return h2
+            else:
+                h1, h2 = l1, l2
+                while not (h1 is h2):
+                    h1 = h1.next
+                    h2 = h2.next
+                return h1
+            pass
+
+        def sol2(l1, l2):
+            h1, h2, stack1, stack2 = l1, l2, [], []
+            while (h1 or h2):
+                if h1:
+                    stack1.append(h1)
+                    h1 = h1.next
+                if h2:
+                    stack2.append(h2)
+                    h2 = h2.next
+            while (stack1[len(stack1) - 1] is stack2[len(stack2) - 1]):
+                stack1.pop()
+                stack2.pop()
+            return stack1[len(stack1) - 1].next
+
+        return sol2(l1, l2)
         pass
 
     # 查找两个链表是否有交点 用栈
     def checkJiaoDian(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        def sol1(l1, l2):
+            h1, h2, stack1, stack2 = l1, l2, [], []
+            while (h1 or h2):
+                if h1:
+                    stack1.append(h1)
+                    h1 = h1.next
+                if h2:
+                    stack2.append(h2)
+                    h2 = h2.next
+            return stack1[len(stack1) - 1] is stack2[len(stack2) - 1]
+
+        return sol1(l1, l2)
 
 
 node1 = ListNode(1)
@@ -36,3 +93,6 @@ sol = Solution()
 # res = sol.mergeKLists([node1, n1])
 res = sol.findJiaoDian(node1, n1)
 print(res.val)
+
+res = sol.checkJiaoDian(node1, n1)
+print(res)
